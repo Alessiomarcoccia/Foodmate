97ab648b7bc37f834e1aac37dd38965bb4ec5cf0
Alessiomarcoccia <80751474+Alessiomarcoccia@users.noreply.github.com>
2023-05-21 13:26:37 +0200

    extended translation and delete button for pantry

---
+++ php/eliminaDispensa.php
@@ -0,0 +1,21 @@
+<?php
+session_set_cookie_params(0);
+session_start();
+$email = $_SESSION['email'];
+$nomeFrigorifero = $_SESSION['nomeFrigorifero']; 
+$dbconn = pg_connect("host=localhost password=Foodmate user=Foodmate port=5432 dbname=progetto")
+    or die("Errore di connessione: " . pg_last_error());
+
+if ($dbconn) {
+
+    $q1 = " delete from dispensa where nome=$1 and utente=$2;";
+    $result = pg_query_params($dbconn, $q1, array($nomeFrigorifero,$email));
+    $response = array('nomeFrigorifero' => $nomeFrigorifero);
+    header('Content-Type: application/json');
+    echo json_encode($response);
+    pg_close($dbconn);
+
+}
+
+
+?>
--- php/register.php
+++ php/register.php
@@ -1,44 +1,35 @@
 <?php
-if ($_SERVER["REQUEST_METHOD"] != "POST") {
-    header("Location: /");
+$dbconn = pg_connect("host=localhost password=Foodmate user=Foodmate port=5432 dbname=progetto")
+    or die("Errore di connessione: " . pg_last_error());
+
+session_set_cookie_params(0);
+session_start();
+if ($dbconn) {
+    $email = $_POST['inputEmail'];
+    $q1 = "select * from utenti where email= $1";
+    $result = pg_query_params($dbconn, $q1, array($email));
+    if ($tuple = pg_fetch_array($result, null, PGSQL_ASSOC)) {
+        $error = "Email gia' registrata";
+        header('Content-Type: application/json');
+        echo json_encode(array('error' => $error));
+    } else {
+        $nome = $_POST['inputName'];
+        $cognome = $_POST['inputSurname'];
+        $psw = $_POST['inputPassword'];
+        $foto = '';
+        $sesso = $_POST['inputSesso'];
+        $q2 = "insert into utenti values ($1,$2,$3,$4,$5,$6)";
+        $data = pg_query_params(
+            $dbconn,
+            $q2,
+            array($nome, $cognome, $email, $psw, $sesso, $foto)
+        );
+        if ($data) {
+            header("Location: ../html/dashboard.html");
+            $_SESSION['email'] = $email;
+        }
+    }
+    pg_close($dbconn);
 }
-else {
-    $dbconn = pg_connect("host=localhost password=Foodmate user=Foodmate port=5432 dbname=progetto") 
-                or die("Errore di connessione: " .pg_last_error()); 
-}
+
 ?>
-<!DOCTYPE html> 
-<html>
-    <head></head>
-    <body>
-        <?php
-            session_set_cookie_params(0);
-            session_start();
-            if ($dbconn) {
-                $email = $_POST['inputEmail'];
-                $q1="select * from utenti where email= $1";
-                $result=pg_query_params($dbconn, $q1, array($email));
-                if ($tuple=pg_fetch_array($result, null, PGSQL_ASSOC)) {
-                    echo "<h1> Spiacente, l'indirizzo email non e' disponibile</h1>
-                        Se vuoi, <a href=../login> clicca qui per loggarti </a>";
-                }
-                else {
-                    $nome = $_POST['inputName'];
-                    $cognome = $_POST['inputSurname'];
-                    $psw = $_POST['inputPassword'];
-                    $foto = '';
-                    $sesso = $_POST['inputSesso'];
-                    $q2 = "insert into utenti values ($1,$2,$3,$4,$5,$6)";
-                    $data = pg_query_params($dbconn, $q2,
-                        array($nome, $cognome, $email, $psw, $sesso, $foto));
-                    if ($data) {
-                       header("Location: ../html/dashboard.html");
-                       $_SESSION['email'] = $email;
-                    }
-                }
-                pg_close($dbconn);
-            }
-            
-        ?> 
-    </body>
-</html>
